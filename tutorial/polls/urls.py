941ce26ae9b73314804f0ace33d371140ab41d7c
James <jgarrison1047@gmail.com>
2021-11-19 11:44:58 -0500

    starting generic views

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+"""from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
@@ -10,5 +11,18 @@ urlpatterns = [
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]"""
+
+
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
