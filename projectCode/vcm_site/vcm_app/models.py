cb767e373dec9c64939fd143d3c7b0416bc95e6a
James <jgarrison1047@gmail.com>
2021-12-01 11:36:11 -0500

    created initial stub models

--- projectCode/vcm_site/vcm_app/models.py
+++ projectCode/vcm_site/vcm_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Vendor(models.Model):
+    vendor_name = models.CharField(max_length=200)
+    #pub_date = models.DateTimeField('date published')
+    vendor_email = models.EmailField(max_length=100) #THIS IS A PLACEHOLDER
+
+
+
+
+class Contract(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.PROTECT) #Does not allow vendor to be deleted
+    contract_title = models.CharField(max_length=200)
+    #votes = models.IntegerField(default=0)
+
+    """ will need to use FileField
+    see https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
+        https://www.geeksforgeeks.org/filefield-django-models/
+        
+    """
