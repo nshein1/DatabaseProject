d40977fd44db2bfb2010403b020878078c11644f
James <jgarrison1047@gmail.com>
2021-12-03 11:28:54 -0500

    added stub views and urls

--- projectCode/vcm_site/vcm_app/urls.py
+++ projectCode/vcm_site/vcm_app/urls.py
@@ -3,5 +3,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # ex: /vcm/
     path('', views.index, name='index'),
+
+
+    # ex: /vcm/vendors/
+    path('vendors/', views.vendors, name='vendors'),
+
+    #ex /vcm/vendors/1/
+    path('vendors/<int:vendor_id>/', views.vendor_detail, name='vendor detail'),
+
+    # ex: /vcm/contracts/
+    path('contracts/', views.contracts, name='contracts'),
+
+     #ex /vcm/contracts/1/
+    path('contracts/<int:contract_id>/', views.contract_detail, name='contract detail'),
 ]
--- projectCode/vcm_site/vcm_app/views.py
+++ projectCode/vcm_site/vcm_app/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the VCM index.")
+
+def vendors(request):
+    return HttpResponse("Hello, world. You're at the vendors page.") 
+
+
+
+def vendor_detail(request, vendor_id):
+    return HttpResponse("You're looking at vendor %s." % vendor_id)
+
+
+def contracts(request):
+    return HttpResponse("Hello, world. You're at the contracts page.") 
+
+
+def contract_detail(request, contract_id):
+    return HttpResponse("You're looking at contract %s." % contract_id)
