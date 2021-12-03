4dcf72638c8e070bcbe58f4592af53b4c9b4904d
James <jgarrison1047@gmail.com>
2021-12-03 16:43:47 -0500

    url namespacing

--- projectCode/vcm_site/vcm_app/urls.py
+++ projectCode/vcm_site/vcm_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'vcm_app'
 urlpatterns = [
 
     # ex: /vcm/
@@ -12,11 +14,11 @@ urlpatterns = [
     path('vendors/', views.vendors, name='vendors'),
 
     #ex /vcm/vendors/1/
-    path('vendors/<int:vendor_id>/', views.vendor_detail, name='vendor detail'),
+    path('vendors/<int:vendor_id>/', views.vendor_detail, name='vendor_detail'),
 
     # ex: /vcm/contracts/
     path('contracts/', views.contracts, name='contracts'),
 
      #ex /vcm/contracts/1/
-    path('contracts/<int:contract_id>/', views.contract_detail, name='contract detail'),
+    path('contracts/<int:contract_id>/', views.contract_detail, name='contract_detail'),
 ]
