8b5acb8f9d70b5ed007b8eb4fc66df9ffa8e9304
James <jgarrison1047@gmail.com>
2021-12-05 06:27:49 -0500

    added search box to vendors page

--- projectCode/vcm_site/vcm_app/views.py
+++ projectCode/vcm_site/vcm_app/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
+
+from django.db.models import Q
+
 from .models import *
 
 # Create your views here.
@@ -11,18 +14,19 @@ def index(request):
     return HttpResponse("Hello, world. You're at the VCM index.")
 
 
-#no long using this version of the method
+"""no long using this version of the method
 def vendors(request):
     #return HttpResponse("Hello, world. You're at the vendors page.") 
 
-    """consider changing this to an order by
-        e.g.   latest_question_list = Question.objects.order_by('-pub_date')[:5]"""
+  
     #vendor_list = Vendor.objects.all() #maybe make this a get_list_or_404()
     vendor_list = Vendor.objects.order_by('vendor_name')
 
     context = {'vendor_list': vendor_list,}
     
     return render(request, 'vcm_app/vendors.html', context)
+"""
+
 
 class VendorsView(generic.ListView):
     template_name = 'vcm_app/vendors.html'
@@ -30,7 +34,16 @@ class VendorsView(generic.ListView):
 
     def get_queryset(self):
         #return Vendor.objects.all()
-        return Vendor.objects.order_by('vendor_name')
+
+        if(search_term := self.request.GET.get("search_term")):
+            return Vendor.objects.filter( Q(vendor_name__icontains=search_term) |
+                                          Q(vendor_email__icontains=search_term)
+                                        ).order_by('vendor_name') #searches single field, vendor_name
+            """to search multiple fields, see https://testdriven.io/blog/django-search/
+                otherwise must add one line for every field"""
+
+        else:
+            return Vendor.objects.order_by('vendor_name') #alphebatize
 
 
 """
