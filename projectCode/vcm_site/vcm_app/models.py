57ac56ace50a7868a1ca84a31c7b1eca700bf65a
nshein1 <93554048+nshein1@users.noreply.github.com>
2021-12-07 18:16:50 -0500

    TypeofWork

--- projectCode/vcm_site/vcm_app/models.py
+++ projectCode/vcm_site/vcm_app/models.py
@@ -6,6 +6,7 @@ class Vendor(models.Model):
     vendor_name = models.CharField(max_length=200)
     #pub_date = models.DateTimeField('date published')
     vendor_email = models.EmailField(max_length=100) #THIS IS A PLACEHOLDER
+    TypeofWorks = models.ManyToManyField(TypeofWork)
 
     #return this vendor's contracts
     def get_contracts(self):
@@ -21,6 +22,7 @@ class Contract(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.PROTECT) #Does not allow vendor to be deleted
     contract_title = models.CharField(max_length=200)
     #votes = models.IntegerField(default=0)
+    TypeofWorks = models.ManyToManyField(TypeofWork)
 
     """Contract Status stuff"""
     CURRENT     = 'CR'
@@ -42,14 +44,25 @@ class Contract(models.Model):
         default=POTENTIAL,
     )
 
+    #toString
+    def __str__(self):
+        return self.contract_title
+
+
+
+
+    class TypeofWork(models.Model):
+        work = models.CharField(max_length=50)
+
+        class Meta:
+            ordering = ['work']
+
+        def __str__(self):
+            return self.work
 
     """ will need to use FileField
     see https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
         https://www.geeksforgeeks.org/filefield-django-models/
 
     """
-
-
-    #toString
-    def __str__(self):
-        return self.contract_title
+""Testing
