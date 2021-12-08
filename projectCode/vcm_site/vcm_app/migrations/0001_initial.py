ee8154325b17a79152f0b3f3b01dedb129c5fa64
James <jgarrison1047@gmail.com>
2021-12-08 12:31:38 -0500

    bunch of small fixes

--- projectCode/vcm_site/vcm_app/migrations/0001_initial.py
+++ projectCode/vcm_site/vcm_app/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('vendor_name', models.CharField(max_length=200)),
                 ('vendor_email', models.EmailField(max_length=100)),
-                ('TypeofWorks', models.ManyToManyField(TypeofWork)),
+                
             ],
         ),
         migrations.CreateModel(
@@ -27,14 +27,14 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('contract_title', models.CharField(max_length=200)),
                 ('vendor', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='vcm_app.vendor')),
-                ('TypeofWorks', models.ManyToManyField(TypeofWork)),
+                
             ],
         ),
         migrations.CreateModel(
             name='Vendor',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('work_type', models.CharField(max_length=50))
+                
             ]
         )
     ]
--- projectCode/vcm_site/vcm_app/models.py
+++ projectCode/vcm_site/vcm_app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from django.core.validators import MinValueValidator
+
 # Create your models here.
 
 class Vendor(models.Model):
     vendor_name = models.CharField(max_length=200)
     #pub_date = models.DateTimeField('date published')
-    vendor_email = models.EmailField(max_length=100) #THIS IS A PLACEHOLDER
-    TypeofWorks = models.ManyToManyField(TypeofWork)
+    vendor_email = models.EmailField(max_length=100)# default="test@test.com") #THIS IS A PLACEHOLDER
+    #TypeofWorks = models.ManyToManyField(TypeofWork)
 
     #return this vendor's contracts
     def get_contracts(self):
@@ -22,7 +24,7 @@ class Contract(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.PROTECT) #Does not allow vendor to be deleted
     contract_title = models.CharField(max_length=200)
     #votes = models.IntegerField(default=0)
-    TypeofWorks = models.ManyToManyField(TypeofWork)
+    #TypeofWorks = models.ManyToManyField(TypeofWork)
 
     """Contract Status stuff"""
     CURRENT     = 'CR'
@@ -49,19 +51,28 @@ class Contract(models.Model):
         return self.contract_title
 
 
+class Payment(models.Model):
+    payment_date = models.DateField()
+    payment_checkNumber = models.IntegerField(validators=[MinValueValidator(1)])
+    payment_checkAmount = models.DecimalField(max_digits=9,decimal_places=2)
+    payment_memo = models.CharField(max_length=500)
+
+    contract = models.ForeignKey(Contract, on_delete=models.PROTECT, default=1)
 
 
-    class TypeofWork(models.Model):
-        work = models.CharField(max_length=50)
 
-        class Meta:
-            ordering = ['work']
+    #this be a toString
+    def __str__(self):
+        return str(self.payment_memo)
 
-        def __str__(self):
-            return self.work
+"""
+class TypeofWork(models.Model):
+    work = models.CharField(max_length=50)
 
-    """ will need to use FileField
-    see https://docs.djangoproject.com/en/3.2/ref/models/fields/#model-field-types
-        https://www.geeksforgeeks.org/filefield-django-models/
+    class Meta:
+        ordering = ['work']
 
-    """
+    def __str__(self):
+        return self.work
+"""
+ 
