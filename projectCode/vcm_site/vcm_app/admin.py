e300bd677db910b91bd6fa29aebe6d49690f0cf9
nshein1 <93554048+nshein1@users.noreply.github.com>
2022-05-10 22:02:34 -0400

    admin registration

--- projectCode/vcm_site/vcm_app/admin.py
+++ projectCode/vcm_site/vcm_app/admin.py
@@ -1,4 +1,7 @@
+import unittest
 from django.contrib import admin
+
+from projectCode.vcm_site.vcm_app.models import Amenities, Building, Inspection, Lease, Maintenance_Request, Parking, Payment_Ledger, Rent_Payment
 
 from .models import *
 
@@ -9,12 +12,24 @@ class VendorAdmin(admin.ModelAdmin):
     ]
 admin.site.register(Vendor, VendorAdmin)
 
+class BuildingAdmin(admin.ModelAdmin):
+    fieldset = [
+        (None,    {'fields': ['vendor_name']}), 
+        ('Email', {'fields': ['vendor_email']}),
+    ]
 
 
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Unit)
+admin.site.register(Person)
+admin.site.register(Lease)
+admin.site.register(Parking)
+admin.site.register(Maintenance_Request)
+admin.site.register(Payment_Ledger)
+admin.site.register(Inspection)
+admin.site.register(Rent_Payment)
+admin.site.register(Amenities)
 
-admin.site.register(Contract)
-admin.site.register(Payment)
-admin.site.register(WorkType)
 # Register your models here.
 
 
